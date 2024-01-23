4e3d93b5e28cddc8457a4b6b3d97e7dc29eb7480
Aubrey Mae Mulawan <aubreymaemulawan@gmail.com>
2024-01-23 22:16:44 +0800

    feature: added system documentation

--- src/storage/framework/views/974a2a23d040cd0fbc127ba4b44e135c7a59f226.php
+++
@@ -1,9 +0,0 @@
-
-
-<?php $__env->startSection('title', __t('payroll_settings')); ?>
-
-<?php $__env->startSection('contents'); ?>
-    <app-payroll-settings-layout></app-payroll-settings-layout>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layout.tenant', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\Laravel-Payroll\src\resources\views/tenant/settings/payroll_settings.blade.php ENDPATH**/ ?>
